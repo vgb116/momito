a8b479860ff71b99b533ab331b6ab9ed46842fca
momitorodriguez <momitorodriguez@404b1668-f4e4-11de-8f43-4547254af6c6>
2010-03-30 22:15:36 +0000

    ya corre

--- trunk/n10_editorDiagramaFlujo/source/uniandes/cupi2/editorDiagramaFlujo/mundo/Entrada.java
+++ trunk/n10_editorDiagramaFlujo/source/uniandes/cupi2/editorDiagramaFlujo/mundo/Entrada.java
@@ -7,35 +7,36 @@ import java.io.PrintWriter;
 import uniandes.cupi2.editorDiagramaFlujo.mundo.IForma;
 import uniandes.cupi2.editorDiagramaFlujo.mundo.Punto;
 
-public class Entrada implements IForma
+public class Entrada extends FormaBasica
 {
+    // -----------------------------------------------------------------
+    // Constantes
+    // -----------------------------------------------------------------
 
-    @Override
-    public void cambiarFuente( Font laFuente )
+    /**
+     * Constante para definir el tipo 
+     */
+    public final static String TIPO = "ENTRADA";
+    
+    /**
+     * Constante para definir el alto
+     */
+    public final static int ALTO = 40;
+    
+    /**
+     * Constante para definir el ancho
+     */
+    public final static int ANCHO = 95;
+    
+    /**
+     * Constante para definir el ancho
+     */
+    public final static int ANCHO_BASE = 20;
+    
+    public Entrada( Punto punto )
     {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void cambiarTexto( String txt )
-    {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public Font darFuente( )
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public String darTexto( )
-    {
-        // TODO Auto-generated method stub
-        return null;
+        
+        super( punto, new Punto( punto.darX( ) + ANCHO, punto.darY( ) + ALTO ) );
     }
 
     @Override
@@ -46,38 +47,32 @@ public class Entrada implements IForma
     }
 
     @Override
-    public boolean estaDentro( Punto punto )
-    {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public void guardar( PrintWriter out )
-    {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
     public void moverFigura( Punto punto )
     {
         // TODO Auto-generated method stub
-
+        
     }
 
     @Override
     public void pintar( Graphics2D g )
     {
         // TODO Auto-generated method stub
-
+        
     }
 
     @Override
     public void pintarSeleccionado( Graphics2D g )
     {
         // TODO Auto-generated method stub
-
+        
     }
 
+    @Override
+    protected void pintarTexto( Graphics2D g )
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
 }
