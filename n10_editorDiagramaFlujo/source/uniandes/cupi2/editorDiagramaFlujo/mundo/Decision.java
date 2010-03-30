08d736c138f68f0a9a41fa48c359339e317a5d15
momitorodriguez <momitorodriguez@404b1668-f4e4-11de-8f43-4547254af6c6>
2010-03-30 21:27:38 +0000

    clase entrada y desicion creadas vacias

---
+++ n10_editorDiagramaFlujo/source/uniandes/cupi2/editorDiagramaFlujo/mundo/Decision.java
@@ -0,0 +1,83 @@
+package uniandes.cupi2.editorDiagramaFlujo.mundo;
+
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.io.PrintWriter;
+
+import uniandes.cupi2.editorDiagramaFlujo.mundo.IForma;
+import uniandes.cupi2.editorDiagramaFlujo.mundo.Punto;
+
+public class Decision implements IForma
+{
+
+    @Override
+    public void cambiarFuente( Font laFuente )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void cambiarTexto( String txt )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Font darFuente( )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String darTexto( )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String darTipo( )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean estaDentro( Punto punto )
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void guardar( PrintWriter out )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void moverFigura( Punto punto )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void pintar( Graphics2D g )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void pintarSeleccionado( Graphics2D g )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
